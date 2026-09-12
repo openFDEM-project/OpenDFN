e6e200eb04b31771dfda01c53d794e92bed50c60
openFDEM-project <140852720+openFDEM-project@users.noreply.github.com>
2026-09-13 00:01:23 +0800

    Delete examples directory

--- examples/discontinuous_joint_sets/generate_formats.ps1
+++
@@ -1,1 +0,0 @@
-& (Join-Path (Split-Path $PSScriptRoot -Parent) "generate_all_formats.ps1") -Case "discontinuous_joint_sets" @args
--- examples/input_real_dfn/generate_formats.ps1
+++
@@ -1,1 +0,0 @@
-& (Join-Path (Split-Path $PSScriptRoot -Parent) "generate_all_formats.ps1") -Case "input_real_dfn" @args
